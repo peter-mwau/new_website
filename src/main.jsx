935b242135512d399da444321969de3650f9e768
Michael Macharia <kimmike1100@gmail.com>
2026-01-25 12:30:33 +0300

    changed site font to space grotesk

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/space-grotesk";
+import "@fontsource/space-grotesk/700.css";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -9,5 +11,5 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
